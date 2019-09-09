adacd4fa7959a78eef963dc8fcff24fd62d05365
hdj6 <473123368@qq.com>
2019-09-09 16:14:15 +0800

    黄东杰第N次提交

--- src/main/java/org/lanqiao/controller/BookController.java
+++ src/main/java/org/lanqiao/controller/BookController.java
@@ -1,11 +1,17 @@
 package org.lanqiao.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
 import org.lanqiao.entity.Books;
 import org.lanqiao.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -16,5 +22,10 @@ public class BookController {
     @RequestMapping("/getBooks")
     public List<Books> selectAllBooks(){
        return  bookService.selectAllBooks();
+    }
+
+    @RequestMapping("solrSelect")
+    public List<Books> queryByCondition(String de) {
+        return bookService.queryByCondition("民国");
     }
 }
--- src/main/java/org/lanqiao/service/BookService.java
+++ src/main/java/org/lanqiao/service/BookService.java
@@ -8,4 +8,5 @@ import java.util.List;
 @Repository
 public interface BookService {
     List<Books> selectAllBooks();
+    List<Books> queryByCondition(String de);
 }
