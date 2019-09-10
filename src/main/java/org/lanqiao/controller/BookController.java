d376e0950ca712c700777ad31a96ce2f215489fc
hdj6 <473123368@qq.com>
2019-09-10 16:36:57 +0800

    黄东杰第N次提交

--- src/main/java/org/lanqiao/controller/BookController.java
+++ src/main/java/org/lanqiao/controller/BookController.java
@@ -13,6 +13,7 @@ import org.lanqiao.service.BookService;
 import org.lanqiao.vo.SelectTypeVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +34,7 @@ public class BookController {
         return bookService.queryByCondition("民国");
     }
     @RequestMapping("/getBooksByType")
-    public String selectBooksByType(SelectTypeVo selectTypeVo, Model model){
+    public String selectBooksByType(@RequestBody SelectTypeVo selectTypeVo, Model model){
         PageHelper.startPage(selectTypeVo.getPageNum(),5);
         List<Books> list = bookService.selectBooksByType(selectTypeVo);
         PageInfo<Books> pageInfo = new PageInfo<Books>(list);
