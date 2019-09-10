e7d50f4b50493fb0d8e0eded3be8d7e04837af37
hdj6 <473123368@qq.com>
2019-09-10 20:19:11 +0800

    黄东杰第N次提交

--- src/main/java/org/lanqiao/controller/BookController.java
+++ src/main/java/org/lanqiao/controller/BookController.java
@@ -2,12 +2,6 @@ package org.lanqiao.controller;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.response.QueryResponse;
-import org.apache.solr.common.SolrDocumentList;
 import org.lanqiao.entity.Books;
 import org.lanqiao.service.BookService;
 import org.lanqiao.vo.SelectTypeVo;
@@ -15,14 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.io.IOException;
 import java.util.List;
 
 @RestController
 public class BookController {
-
     @Autowired
     BookService bookService;
     @RequestMapping("/getBooks")
